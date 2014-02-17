bf9434647899cdd0ea3234c9589e053e0acef71a
lengshuiyulangcn <gyorou@tjjtds.me>
2014-02-17 12:05:41 +0900

    fix a bug of the return value

--- lib/cseg.rb
+++ lib/cseg.rb
@@ -3,25 +3,19 @@ require "cseg/version"
 require "tempfile"
 class Kurumi
 	# since crf++ can only read from file
-	#@tmpfile.read{|file| file=nil}
 	@modle=File.expand_path("../../data/pkumodle.data", __FILE__)
-	@result=Array.new
 	def self.segment(str)
 		tmpstr=""
 		for i in 0..str.length-1
 			tmpstr+=str[i]+"\n"
 		end
-		#@tmpfile=nil
 	@tmp=Tempfile::new("tmp")
 	@resultfile=Tempfile::new("result")
-		@tmp.write(tmpstr)
-		@tmp.rewind
-		#@tmpfile.close
-		system("crf_test -m #{@modle} #{@tmp.path}>#{@resultfile.path}")
-		@resultfile.rewind
-	#	puts @resultfile.read
-	#	puts @tmp.read
-		
+	@tmp.write(tmpstr)
+	@tmp.rewind
+	@result=Array.new
+	system("crf_test -m #{@modle} #{@tmp.path}>#{@resultfile.path}")
+	@resultfile.rewind
 		word=""
 		@resultfile.read.each_line{|line|
 			token=line.chomp.split("	")
@@ -38,16 +32,14 @@ class Kurumi
 					@result.push(word)
 				end
 			end
-			# @result.push(line.chomp.split("	")[0])
 
 		}
 		
 		@resultfile.close(true)
 		@tmp.close(true)
 		return @result
-		# puts $?
 	end
 end
 
-# result=Cseg.segment("ˆ¢çŒ?z??èÜC¥ˆêŠ¼œk??“I??èÜ")
+# result=Cseg.segment("å±Œä¸æ˜¯ä¸€ç§ç”Ÿæ´»æ€åº¦")
 # print result
