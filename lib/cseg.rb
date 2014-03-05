ec685df902d2311b8ff90f2f6c08a15b8b2172bb
lengshuiyulangcn <gyorou@tjjtds.me>
2014-03-05 17:00:22 +0900

    use pipe instead of tempfile

--- lib/cseg.rb
+++ lib/cseg.rb
@@ -5,19 +5,14 @@ class Kurumi
 	# since crf++ can only read from file
 	@modle=File.expand_path("../../data/pkumodle.data", __FILE__)
 	def self.segment(str)
-		tmpstr=""
-		for i in 0..str.length-1
-			tmpstr+=str[i]+"\n"
-		end
-	@tmp=Tempfile::new("tmp")
-	@resultfile=Tempfile::new("result")
-	@tmp.write(tmpstr)
-	@tmp.rewind
-	@result=Array.new
-	system("crf_test -m #{@modle} #{@tmp.path}>#{@resultfile.path}")
-	@resultfile.rewind
+	  @result=Array.new
+	  result_data = IO.popen "crf_test -m #{@modle}", 'r+' do |io|
+  		io.puts *str.chars
+  		io.close_write
+  		io.read
+	  end
 		word=""
-		@resultfile.read.each_line{|line|
+		result_data.each_line{|line|
 			token=line.chomp.split("	")
 			if token[1]=="B"or token[1]=="O"
 				if word!=""
@@ -35,12 +30,8 @@ class Kurumi
 			end
 
 		}
-		
-		@resultfile.close(true)
-		@tmp.close(true)
 		return @result
 	end
 end
-
-# result=Cseg.segment("屌丝是一种生活态度")
+# result=Kurumi.segment("屌丝是一种生活态度")
 # print result
